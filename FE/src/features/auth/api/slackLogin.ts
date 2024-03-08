88580eea9bd6083b4874495c7f7ca4075d0608b3
bada308 <203190@jnu.ac.kr>
2024-03-08 15:52:59 +0900

    refactor: auth feature 관련 api 구현

---
+++ FE/src/features/auth/api/slackLogin.ts
@@ -0,0 +1,56 @@
+import { useMutation } from "@tanstack/react-query";
+import { useRouter } from "next/navigation";
+import { TokenDto } from "../types";
+import API from "@/constants/API";
+import ERROR_CODE from "@/constants/ERROR_CODE";
+import ROUTES from "@/constants/ROUTES";
+import { https } from "@/lib/axios";
+import storage from "@/utils/storage";
+
+interface SlackLoginRequest {
+  code: string;
+  redirect_uri: string;
+}
+
+export class SlackLoginDto extends TokenDto {}
+
+export const slackLogin = async ({
+  code,
+  redirect_uri,
+}: SlackLoginRequest): Promise<SlackLoginDto> => {
+  const { data } = await https({
+    url: API.AUTH.SLACK_LOGIN,
+    method: "POST",
+    params: { code, redirect_uri },
+  });
+
+  return new SlackLoginDto(data?.data);
+};
+
+export const useSlackLogin = () => {
+  const router = useRouter();
+  useMutation({
+    mutationFn: slackLogin,
+  });
+
+  return useMutation(
+    [API.AUTH.SLACK_LOGIN],
+    async (request: SlackLoginRequest) => {
+      return await slackLogin(request);
+    },
+    {
+      onSuccess: (data) => {
+        const { accessToken, accessExpiredTime } = data;
+        storage.setToken(accessToken);
+        storage.setTokenExpiration(accessExpiredTime);
+
+        router.replace(ROUTES.MAIN);
+      },
+      onError: (error: any) => {
+        const errorCode = error?.response?.data?.code;
+        errorCode === ERROR_CODE.AUTH.INVALID_NAME &&
+          router.replace(ROUTES.NAME_ERROR);
+      },
+    },
+  );
+};
---
+++ FE/src/features/auth/api/tokenReissue.ts
@@ -0,0 +1,13 @@
+import { TokenDto } from "../types";
+import API from "@/constants/API";
+import { https } from "@/lib/axios";
+
+export class TokenReissueDto extends TokenDto {}
+
+export const tokenReissue = async (): Promise<TokenReissueDto> => {
+  const { data } = await https({
+    url: API.AUTH.TOKEN_REISSUE,
+    method: "POST",
+  });
+  return new TokenReissueDto(data?.data);
+};
