fc3618bae220e9055107260ac347cda3ba49a03a
bada308 <203190@jnu.ac.kr>
2024-03-08 19:24:07 +0900

    refactor: attendStatus 관련 컴포넌트, utils 이동

---
+++ FE/src/features/users/components/attendStatus/Editor.tsx
@@ -0,0 +1,53 @@
+import { useUpdateAttendStatus } from "../../apis/updateAttendStatus";
+import { getDisplayInfo, getEditableStatus } from "../../utils/attendStatus";
+import UserAttendStatusEditorLoader from "./Editor.loader";
+import AttendStatusToggle from "@/components/common/attendStatusToggle/AttendStatusToggle";
+import StatusToggleItem from "@/components/common/attendStatusToggle/StatusToggleItem";
+import ATTEND_STATUS from "@/constants/ATTEND_STATUS";
+import MESSAGE from "@/constants/MESSAGE";
+import useGetAttendStatusAndProgramInfo from "@/hooks/useGetAttendStatusAndProgramInfo";
+import { AttendStatus } from "@/types/member";
+
+interface UserAttendStatusEditorProps {
+  programId: number;
+}
+
+const UserAttendStatusEditor = ({ programId }: UserAttendStatusEditorProps) => {
+  const {
+    userAttendStatusInfo: { name, attendStatus },
+    isLoading,
+    programType,
+    programStatus,
+  } = useGetAttendStatusAndProgramInfo(programId);
+  const { mutate: updateAttendStatus } = useUpdateAttendStatus(programId);
+
+  if (isLoading) return <UserAttendStatusEditorLoader />;
+
+  const { displayText, color } = getDisplayInfo(programType, attendStatus);
+  const editableStatus = getEditableStatus(attendStatus, programStatus);
+
+  const handleSelectorClick = (value: AttendStatus) => {
+    confirm(MESSAGE.CONFIRM.EDIT) &&
+      updateAttendStatus({
+        beforeAttendStatus: attendStatus,
+        afterAttendStatus: value,
+      });
+  };
+
+  return (
+    <>
+      <div className="mb-4 flex items-center gap-4">
+        <p className="text-lg font-semibold">{name}</p>
+        <StatusToggleItem text={displayText} color={color} />
+      </div>
+      <p className="mb-2">{ATTEND_STATUS.LABEL[editableStatus]}</p>;
+      <AttendStatusToggle
+        selectedValue={attendStatus}
+        disabled={editableStatus !== "EDITABLE"}
+        onSelect={(v) => handleSelectorClick(v)}
+      />
+    </>
+  );
+};
+
+export default UserAttendStatusEditor;
