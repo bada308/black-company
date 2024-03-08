27052591cb90d758645f4117ae6f4944379b2ca3
bada308 <203190@jnu.ac.kr>
2024-03-08 19:25:04 +0900

    refactor: user 관련 Editor 컴포넌트 export

--- FE/src/features/users/index.ts
+++ FE/src/features/users/index.ts
@@ -3,4 +3,7 @@ export * from "./apis/getAttendStatus";
 export * from "./apis/updateActiveStatus";
 export * from "./apis/updateAttendStatus";
 
+export * from "./components/attendStatus/Editor";
+export * from "./components/activeStatus/Editor";
+
 export * from "./types";
