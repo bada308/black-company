fbe61571b5cfc532d9ade560c232b6a95adac60f
bada308 <203190@jnu.ac.kr>
2024-03-08 17:03:54 +0900

    feat: auth 기능 공통 타입 선언

---
+++ FE/src/features/auth/types/index.ts
@@ -0,0 +1,9 @@
+export class TokenDto {
+  public readonly accessToken: string;
+  public readonly accessExpiredTime: number;
+
+  constructor(data: { accessToken: string; accessExpiredTime: number }) {
+    this.accessToken = data?.accessToken;
+    this.accessExpiredTime = data?.accessExpiredTime;
+  }
+}
