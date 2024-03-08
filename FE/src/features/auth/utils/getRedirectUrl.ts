53c6e374e99ec5c3e3edc440313e56af0f8265c1
bada308 <203190@jnu.ac.kr>
2024-03-08 17:02:08 +0900

    feat: sns 종류 별 리다이렉트 url을 구하는 함수 구현

---
+++ FE/src/features/auth/utils/getRedirectUrl.ts
@@ -0,0 +1,22 @@
+export const getRedirectUrl = (provider: string) => {
+  console.log("provider", provider);
+  switch (provider) {
+    case "slack": {
+      const clientId = process.env.NEXT_PUBLIC_SLACK_CLIENT_ID;
+      const redirectUri = process.env.NEXT_PUBLIC_SLACK_REDIRECT_URI;
+      const teamId = process.env.NEXT_PUBLIC_TEAM_ID;
+
+      console.log("clientId", clientId);
+      console.log("redirectUri", redirectUri);
+      console.log("teamId", teamId);
+
+      if (!clientId || !redirectUri || !teamId) {
+        throw new Error("Environment variables are not set");
+      }
+
+      return `https://slack.com/oauth/v2/authorize?client_id=${clientId}&scope=identity.basic&redirect_uri=${redirectUri}&state=${teamId}`;
+    }
+    default:
+      throw new Error(`Unknown provider: ${provider}`);
+  }
+};
