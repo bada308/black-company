a2c9de1faaa7e9bac0efb93a44634ef3870e2025
bada308 <203190@jnu.ac.kr>
2024-03-08 17:06:30 +0900

    delete: 기존 auth 관련 api 코드 제거

--- FE/src/apis/dtos/auth.dto.ts
+++
@@ -1,8 +0,0 @@
-export class LoginDto {
-  public readonly accessToken: string;
-  public readonly accessExpiredTime: number;
-  constructor(data: { accessToken: string; accessExpiredTime: number }) {
-    this.accessToken = data?.accessToken;
-    this.accessExpiredTime = data?.accessExpiredTime;
-  }
-}
